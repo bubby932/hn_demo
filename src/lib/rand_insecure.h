6bdf93f66f095786ad542a54ff14480477f43d52
Rose <82341739+bubby932@users.noreply.github.com>
2023-01-15 21:17:41 -0700

    mhm

--- src/lib/rand_insecure.h
+++ src/lib/rand_insecure.h
@@ -16,7 +16,7 @@ void insec_rand_seed() {
     debug_terminal_writestring("[RAND] Seeding random number generator...\n");
     current = (uint32_t)(rdtsc() >> 32);
     io_wait_long();
-    current ^= (uint32_t)rdtsc();
+    current *= (uint32_t)rdtsc();
     for (size_t i = 0; i < 50; i++)
         insec_rand_next();
     
