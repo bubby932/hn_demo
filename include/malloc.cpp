fbc1b497757c5da26fb46edc96a2091798c59749
Raven <tvrec10@gmail.com>
2023-03-08 16:44:47 -0700

    Kernel: Started work on malloc()
    - Started working on the new virtual memory allocator.

--- include/malloc.cpp
+++ include/malloc.cpp
@@ -1,23 +1,37 @@
 #include <stdlib.h>
+#include "must/maybe.h"
+#include "must/verify.h"
 
-/// @brief Used for managing allocations internally.
-typedef struct _Allocation {
-	size_t 	size;
-	bool 	free;
+namespace must {
+	/// @brief Used for managing allocations internally.
+	typedef struct _Allocation {
+		size_t 	size;
+		bool 	free;
 
-	struct _Allocation *next;
-} Allocation;
+		struct _Allocation *next;
+	} Allocation;
 
-class Heap {
-	private:
-		Allocation *base;
+	class Heap {
+		public:
+			static Maybe<Heap> create(void *start, size_t size) {
+				// Verify that the start address is valid.
+				if(!verify_kernel_data_address(start))
+					return Maybe<Heap>::none();
+				// Verify that the end address is valid.
+				if(!verify_kernel_data_address(start + size))
+					return Maybe<Heap>::none();
 
-	public:
-	Heap(void *start, size_t size) {
-		if(size <= sizeof(Allocation))
-		base = (Allocation *)start;
-		base->size = size - sizeof(Allocation);
-		base->free = true;
-		base->next = NULL;
-	}
-};
+				return Maybe<Heap>::some(Heap(start, size));
+			}
+
+		private:
+			Heap(void *start, size_t size) {
+				base = (Allocation *)start;
+				base->size = size - sizeof(Allocation);
+				base->free = true;
+				base->next = NULL;
+			}
+
+			Allocation *base;
+	};
+}
